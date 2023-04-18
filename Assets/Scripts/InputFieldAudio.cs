b1043ca3793a9266ab5f36f207c63b63d96f5189
Aymen Hammami <hammami.aym@outlook.com>
2023-04-18 12:13:21 +0100

    finished hackathon, commit for prize money

--- Assets/Scripts/InputFieldAudio.cs
+++ Assets/Scripts/InputFieldAudio.cs
@@ -1,8 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 
-    public class InputFieldAudio : MonoBehaviour
-    {
+public class InputFieldAudio : MonoBehaviour
+{
+    public TargetManager targetManager;
+    private bool clicked = false;
+        private void Update()
+        {
+            if (Input.GetKeyDown("return") && !clicked && targetManager.runItButton.interactable)
+            {
+                targetManager.RunIt();
+                clicked = true;
+            }
+            else
+            {
+                clicked = false;
+            }
+        }
+
         public void PlayKeyClip()
         {
             BackgroundMusic.Instance.PlayKeyClip();
---
+++ README.md
@@ -0,0 +1,5 @@
+ROBOREPAIR was our submission for the 24H Game Development Hackathon hosted by SMU - MedTech (15-16 April 2023).
+
+We won Second Prize! More abt it here: https://www.linkedin.com/posts/aymendps_gamedevelopment-university-medtech-activity-7053399477331591168-Ikh9?utm_source=share&utm_medium=member_desktop
+
+PS: This code was done in 24 hours and is a MESS, since we had to deliver everything in a short period of time. I might revisit this code and refactor it in the future, but for now, navigate it at your own risk!
